e0a8de80b0e7145545777a772879d55f1350d29c
yuyetingxiao <2939708850@qq.com>
2025-11-06 19:29:55 +0800

    AssessmentOne.java

--- src/AssessmentOne.java
+++ src/AssessmentOne.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
 public class AssessmentOne {
+ public static void main(String[] args) {
+    System.out.println("=== Part 3: Health Professional Objects ===");
 
+    GeneralPractitioner gp1 = new GeneralPractitioner(101, "Dr. Lisa", "Family Medicine", true);
+    GeneralPractitioner gp2 = new GeneralPractitioner(102, "Dr. Mike", "Family Medicine", false);
+    GeneralPractitioner gp3 = new GeneralPractitioner(103, "Dr. Sarah", "Family Medicine", true);
+
+    Cardiologist cardio1 = new Cardiologist(201, "Dr. John", "Cardiology", "Coronary Heart Disease");
+    Cardiologist cardio2 = new Cardiologist(202, "Dr. Emma", "Cardiology", "Arrhythmia");
+
+    gp1.printGPDetails();
+    System.out.println("-----");
+    gp2.printGPDetails();
+    System.out.println("-----");
+    gp3.printGPDetails();
+    System.out.println("-----");
+    cardio1.printCardioDetails();
+    System.out.println("-----");
+    cardio2.printCardioDetails();
+
+    System.out.println("------------------------------");
+}
 }
